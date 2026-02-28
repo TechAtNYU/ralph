56de275de0f34ec2703d0402837dc0585b619103
Chenxin Yan <yanchenxin2004@gmail.com>
2026-02-28 02:57:05 -0500

    lint

--- apps/docs/src/lib/layout.shared.tsx
+++ apps/docs/src/lib/layout.shared.tsx
@@ -1,16 +1,16 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
+  user: "fuma-nama",
+  repo: "fumadocs",
+  branch: "main",
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'Ralph',
+      title: "Ralph",
     },
     githubUrl: `https://github.com/TechAtNYU/ralph`,
   };
