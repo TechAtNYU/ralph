b47be6307386be789137afae6ad2bec5485d7ee5
Chenxin Yan <yanchenxin2004@gmail.com>
2026-03-11 18:51:59 -0400

    clean up index.ts

--- packages/daemon/src/index.ts
+++ packages/daemon/src/index.ts
@@ -1,22 +1,9 @@
 export { DaemonClient, daemon } from "./client";
-export * from "./env";
 export {
 	ensureDaemonRunning,
-	resolveDaemonLaunchSpec,
-	resolveSiblingDaemonPath,
 	runForegroundDaemon,
-	shouldAutoStartDaemon,
 	startDetached,
 	stopDaemon,
 	waitUntilReady,
 } from "./launcher";
-export * from "./opencode";
-export * from "./protocol";
-export {
-	clearStaleSocket,
-	createConnectionHandler,
-	Daemon,
-	ensureSocketDir,
-	runDaemonServer,
-} from "./server";
-export * from "./store";
+export type * from "./protocol";
