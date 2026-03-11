581f5692d9bdaee2ec5b63189657ab3117d9de62
Chenxin Yan <yanchenxin2004@gmail.com>
2026-03-11 17:00:32 -0400

    cleanup

--- scripts/release/build-binaries.ts
+++ scripts/release/build-binaries.ts
@@ -1,17 +1,13 @@
 import {
 	buildBinaries,
 	DEFAULT_COMPILED_DIR,
-	getCurrentTarget,
-	hasFlag,
 	parseTargets,
 	readFlagValue,
 } from "./shared";
 
 const argv = process.argv.slice(2);
 const outDir = readFlagValue(argv, "--outdir") ?? DEFAULT_COMPILED_DIR;
-const targets = hasFlag(argv, "--current")
-	? [getCurrentTarget()]
-	: (parseTargets(argv) ?? undefined);
+const targets = parseTargets(argv) ?? undefined;
 
 await buildBinaries({
 	outDir,
